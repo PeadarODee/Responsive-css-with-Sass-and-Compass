8d256a53fb0e2b46106a9d739af5e544c5835a04
Peter Donnelly <peterdonnellydesign@gmail.com>
2016-08-15 01:11:50 +0100

    Added watch task for html scripts andcreated default task

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,3 +1,3 @@
 function test() {
-    return "hello";
+    return "hello ya hoooooorrryaaa!!";
 }
--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.initConfig({
         uglify: {
             my_target: {
@@ -7,6 +8,17 @@ module.exports = function(grunt) {
                     '_/js/script.js': ['_/components/js/*.js']
                 } //files
             } //my_Target
-        } //uglify
+        }, //uglify
+        watch: {
+            options: { livereload: true },
+            scripts: {
+                files: ['_/components/js/*.js'],
+                tasks: ['uglify']
+            }, //scripts
+            html: {
+                files: ['*.html']
+        }, //html
+    } //watch
     }) //initConfig
+    grunt.registerTask('default', 'watch');
 } //exports
